a3b612ebd4ba079a5be4408938b6b6c471fa2196
purple-affogato <ivy.yiting.zhuang@gmail.com>
2025-12-31 14:53:59 -0500

    text and title for about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,14 @@
+import '../css/About.css';
+
 function About() {
     return (
         <section id="about">
-
+            <div className='about-title'>
+                <h2>About</h2>
+            </div>
+            <p>
+                Hack for Humanity is an opportunity to identify and solve pressing social issues using your creativity and code. We host a series of workshops, bring experienced mentors, and create networking opportunities with industry professionals. Come enjoy the free food, free merch, and exciting events planned!
+            </p>
         </section>
     );
 }
