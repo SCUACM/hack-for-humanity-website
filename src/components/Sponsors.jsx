99802a4bd48344d794863d9f9ca62dfdd1363538
purple-affogato <ivy.yiting.zhuang@gmail.com>
2026-01-18 23:38:18 -0800

    start of sponsors ig idk man

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,6 +1,19 @@
+import '../css/Sponsors.css';
+import blueWashiTape from '../assets/images/bluewashitape.png';
+import starSticker from '../assets/images/star_sticker_border.png';
+import rocketImg from '../assets/images/rocket.png';
+
 function Sponsors() {
     return (
-        <section id="sponsors"></section>
+        <section id="sponsors">
+            <img src={blueWashiTape}></img>
+            <img src={starSticker} className='sticker star1'></img>
+            <img src={starSticker} className='sticker star2'></img>
+            <img src={rocketImg} className='sticker rocket'></img>
+            <p>Thank you to all our</p>
+            <h1>Sponsors!</h1>
+
+        </section>
     );
 }
 
