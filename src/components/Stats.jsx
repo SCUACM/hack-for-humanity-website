079a6d2704da5fae0a78839f521fd8334e6296fe
Kaavya Jethwa <98070317+k-jethwa@users.noreply.github.com>
2026-01-20 21:34:01 -0800

    changed stats a bit

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -10,35 +10,30 @@ function Stats() {
   return (
     <section id="stats">
       <div className='green-bar'></div>
-      <h2>LAST YEAR...</h2>
-      
+      <h2>BY THE NUMBERS</h2>
       <div className='stats-container'>
         <div className='stats-row'>
           <div className='stat-card'>
             <img src={pictureOne} alt="Stat visual" className='stat-image'/>
-            <p className='stat-text'>300+<br/>Hackers</p>
+            <p className='stat-text'>300+<br/>passionate builders</p>
           </div>
-          
           <div className='stat-card'>
             <img src={pictureTwo} alt="Stat visual" className='stat-image'/>
-            <p className='stat-text'>9th<br/>year</p>
+            <p className='stat-text'>13 years<br/>of innovation</p>
           </div>
-          
           <div className='stat-card'>
             <img src={pictureThree} alt="Stat visual" className='stat-image'/>
-            <p className='stat-text'>$10000+ in<br/>prizes</p>
+            <p className='stat-text'>$10,000+<br/>in prizes</p>
           </div>
         </div>
-        
         <div className='stats-row'>
           <div className='stat-card'>
             <img src={pictureFive} alt="Stat visual" className='stat-image'/>
-            <p className='stat-text'>50+<br/>submissions</p>
+            <p className='stat-text'>50+<br/>groundbreaking projects</p>
           </div>
-          
           <div className='stat-card'>
             <img src={pictureFour} alt="Stat visual" className='stat-image'/>
-            <p className='stat-text'>20+<br/>industry judges</p>
+            <p className='stat-text'>20+<br/>expert judges</p>
           </div>
         </div>
       </div>
